3407d3a5c41c7712c2ff50e656c54c35e994221f
Bofta <montassar367@gmail.com>
2024-09-07 17:04:36 +0200

    Last Working version of navbar + items

--- frontend/pages/Auth/Navigation.jsx
+++ frontend/pages/Auth/Navigation.jsx
@@ -11,7 +11,6 @@ import { Link } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
 import "./Navigation.css";
 
-
 const Navigation = () => {
   const [dropdownOpen, setDropdownOpen] = useState(false);
   const [showSidebar, setShowSidebar] = useState(false);
@@ -20,18 +19,20 @@ const Navigation = () => {
     setDropdownOpen(!dropdownOpen);
   };
 
- 
-  const navigate = useNavigate();
+  const toggleSidebar = () => {
+    setDropdownOpen(!showSidebar);
+  }
 
-
- 
+  const closeSidebar = () => {
+    setShowSidebar(!showSidebar);
+  }
 
   return (
     <div
       style={{ zIndex: 9999 }}
       className={`${
         showSidebar ? "hidden" : "flex"
-      } xl:flex lg:flex md:hidden sm:hidden flex-col justify-between p-4 text-white bg-[#000] w-[4%] hover:w-[15%] h-[100vh]  fixed bottom-0`}
+      } xl:flex lg:flex md:hidden sm:hidden flex-col justify-between p-4 text-white bg-[#000] w-[4%] hover:w-[15%] h-[100vh]  fixed `}
       id="navigation-container"
     >
       <div className="flex flex-col justify-center space-y-4">
@@ -58,37 +59,40 @@ const Navigation = () => {
           </div>
         </Link>
 
-        <Link to="/favorite" className="flex relative">
-          <div className="flex justify-center items-center transition-transform transform hover:translate-x-2">
-            <FaHeart className="mt-[3rem] mr-2" size={20} />
-            <span className="hidden nav-item-name mt-[3rem]">
-              Favorites
-            </span>{" "}
-        
+        <Link to="/favorite">
+          <div className="flex items-center transition-transform transform hover:translate-x-2">
+            <FaHeart className="mr-2 mt-[3rem]" size={26} />
+            <span className="hidden nav-item-name mt-[3rem]">FAVORITE</span>{" "}
           </div>
         </Link>
-      </div>
-      <ul className="flex flex-col space-y-2 mt-auto">
-        <li>
-          <Link
-            to="/login"
-            className="flex items-center transition-transform transform hover:translate-x-2"
-          >
-            <AiOutlineLogin className="mr-2 mt-[4px]" size={26} />
-            <span className="hidden nav-item-name">LOGIN</span>
-          </Link>
-        </li>
-        <li>
-          <Link
-            to="/register"
-            className="flex items-center transition-transform transform hover:translate-x-2"
-          >
-            <AiOutlineUserAdd size={26} />
-            <span className="hidden nav-item-name">REGISTER</span>
-          </Link>
-          </li> 
-          </ul>      
-      </div>
-  );
-};      
+    </div>
+        
 
+    <ul>
+            <li>
+              <Link
+                to="/login"
+                className="flex items-center mt-5 transition-transform transform hover:translate-x-2"
+              >
+                <AiOutlineLogin className="mr-2 mt-[4px]" size={26} />
+                <span className="hidden nav-item-name">LOGIN</span>
+              </Link>
+            </li>
+            <li>
+              <Link
+                to="/register"
+                className="flex items-center mt-5 transition-transform transform hover:translate-x-2"
+              >
+                <AiOutlineUserAdd size={26} />
+                <p className="hidden nav-item-name">REGISTER</p>
+              </Link>
+            </li>
+          </ul>
+        
+  
+  </div>
+
+   );
+  };
+
+export default Navigation;
