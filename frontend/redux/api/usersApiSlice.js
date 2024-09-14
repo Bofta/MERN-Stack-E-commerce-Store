518f0105a70ea37c7d826eb1f4b39f0118bf55af
Bofta <montassar367@gmail.com>
2024-09-14 17:15:28 +0200

    Fhinished the Login/Register ( Working properly )

--- frontend/redux/api/usersApiSlice.js
+++ frontend/redux/api/usersApiSlice.js
@@ -10,8 +10,21 @@ export const userApiSlice = apiSlice.injectEndpoints({
           body: data,
             }),
         }),
+      logout: builder.mutation({
+          query: () => ({
+            url: `${USERS_URL}/logout`,
+            method: "POST",
+          }),
+        }),
+      register: builder.mutation({
+        query: (data) => ({
+          url: `${USERS_URL}`,
+          method: "POST",
+          body: data,
+        }),
+      }),
     }),
 });
 
-export const { useLoginMutation, useLogoutMutation } = userApiSlice;
+export const { useLoginMutation, useLogoutMutation, useRegisterMutation} = userApiSlice;
 
--- frontend/src/pages/Auth/Login.jsx
+++ frontend/src/pages/Auth/Login.jsx
@@ -4,6 +4,7 @@ import { useSelector, useDispatch } from "react-redux";
 import { useLoginMutation } from "../../../redux/api/usersApiSlice.js";
 import { setCredentials } from "../../../redux/features/auth/authSlice";
 import { toast } from "react-toastify";
+import Loader from "../../components/Loader.jsx";
 
 
 const Login = () => {
@@ -15,9 +16,9 @@ const Login = () => {
 
     const [login, { isLoading }] = useLoginMutation();
 
-    const { userInfo } = useSelector((state) => state.auth); // get user info from redux state
+    const {userInfo} = useSelector(state => state.auth); // get user info from redux state
 
-    const { search } = useLocation();
+    const {search} = useLocation();
     const sp = new URLSearchParams(search);
     const redirect = sp.get('redirect') || '/';
 
@@ -27,15 +28,97 @@ const Login = () => {
         }
     }, [navigate, redirect, userInfo]);
 
+    const submitHandler = async (e) => {
+        e.preventDefault();
+        try {
+            const res = await login({email, password}).unwrap();
+            console.log(res);
+            dispatch(setCredentials({...res}));
+        } catch (error) {
+            toast.error(error?.data?.message || error.error);
+        }
+    }
+
 
 
   return (
     <div>
-      <section className="pl-[10rem] flex flex-wrap">
-        <div>
+     <section className="pl-[10rem] flex flex-wrap">
+        <div className="mr-[4rem] mt-[5rem]">
           <h1 className="text-2xl font-semibold mb-4">Sign In</h1>
+
+
+        <form onSubmit={submitHandler} className="container w-[40rem]">
+            <div className="my-[2rem]">
+                <label 
+                htmlFor="email"
+                className="block text-sm font-medium text-white"
+            >
+                Email Address        
+                </label>
+
+                <input
+                    type="email"
+                    id="email"
+                    className="mt-1 p-2 border rounded w-full" 
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                />
+            </div>
+
+
+
+            <div className="my-[2rem]">
+                <label htmlFor="password"
+                className="block text-sm font-medium text-white"
+            >
+                Password   
+                </label>
+
+                <input
+                    type="password"
+                    id="password"
+                    className="mt-1 p-2 border rounded w-full" 
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                />
+            </div>
+
+            <button disabled={isLoading} type="submit" 
+            className="bg-pink-500 text-white px-4 py-2 rounded cursor-pointer my-[1rem]"
+            >
+                {isLoading ? "Loading..." : "Sign In"}
+            </button>
+
+            {isLoading && <Loader />}
+
+
+        <div className="mt-4">
+            <p className="text-white">
+                New Customer ? {" "}
+                <Link to={redirect ? `/register?redirect=${redirect}` : "/register"} 
+                className="text-pink-500 hover:underline"
+                >
+                    Register
+                </Link>
+            </p>
         </div>
+
+
+
+        </form>
+
         
+
+
+
+
+
+
+
+
+
+        </div>
       </section>
     </div>
   )
