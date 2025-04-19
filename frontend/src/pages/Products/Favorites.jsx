0f7a53bafac82fe01dc35039e3975bd24442ddc4
Montasser Ben Rejeb <montassar367@gmail.com>
2025-04-19 16:50:58 +0200

    Added ProductDetails + Productstabs/ ratings and reviews

--- frontend/src/pages/Products/Favorites.jsx
+++ frontend/src/pages/Products/Favorites.jsx
@@ -9,7 +9,18 @@ const Favorites = () => {
     console.log(favorites);
   
 return (
-    <div>  </div> 
+    <div className="ml-[10rem]">
+    <h1 className="text-lg font-bold ml-[3rem] mt-[3rem]">
+        FAVORITE PRODUCTS
+    </h1>    
+    
+
+    <div className="flex flex-wrap">
+        {favorites.map((product) => (
+            <Product key={product._id} product={product} />
+        ))}
+        </div>
+    </div>
 );
 };
 
